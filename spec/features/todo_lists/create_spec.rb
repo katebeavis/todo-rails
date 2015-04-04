c8575759ac51370ae182573fd5e01b500c01987c
Kate <katebeavis84@gmail.com>
2015-04-04 23:57:06 +0100

    Added presence and minimum length for description and passing tests

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -26,18 +26,46 @@ describe 'Creating todo lists' do
     expect(page).to_not have_content('This is what I\'m doing today')
   end
 
+    it 'displays an error when the todo list has no description' do
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      click_link 'New Todo list'
+      expect(page).to have_content('New Todo List')
+      fill_in 'Title', with: 'Hello'
+      fill_in 'Description', with: ''
+      click_button 'Create Todo list'
+      expect(page).to have_content('error')
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      expect(page).to_not have_content('Hello')
+  end
+
     it 'displays an error when the todo list has a title less than 3 characters' do
-    expect(TodoList.count).to eq(0)
-    visit '/todo_lists'
-    click_link 'New Todo list'
-    expect(page).to have_content('New Todo List')
-    fill_in 'Title', with: 'Hi'
-    fill_in 'Description', with: 'This is what I\'m doing today'
-    click_button 'Create Todo list'
-    expect(page).to have_content('error')
-    expect(TodoList.count).to eq(0)
-    visit '/todo_lists'
-    expect(page).to_not have_content('This is what I\'m doing today')
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      click_link 'New Todo list'
+      expect(page).to have_content('New Todo List')
+      fill_in 'Title', with: 'Hi'
+      fill_in 'Description', with: 'This is what I\'m doing today'
+      click_button 'Create Todo list'
+      expect(page).to have_content('error')
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      expect(page).to_not have_content('This is what I\'m doing today')
+  end
+
+    it 'displays an error when the todo list has a description less than 3 characters' do
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      click_link 'New Todo list'
+      expect(page).to have_content('New Todo List')
+      fill_in 'Title', with: 'Hello'
+      fill_in 'Description', with: 'Hi'
+      click_button 'Create Todo list'
+      expect(page).to have_content('error')
+      expect(TodoList.count).to eq(0)
+      visit '/todo_lists'
+      expect(page).to_not have_content('Hi')
   end
 
 end
