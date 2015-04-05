4f22b96cac00bb701538007d5e4e5578b972f56f
Kate <katebeavis84@gmail.com>
2015-04-05 14:31:37 +0100

    Added destroy spec and passing tests

--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 
 RSpec.describe TodoListsController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # TodoList. As you add validations to TodoList, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
@@ -14,9 +11,6 @@ RSpec.describe TodoListsController, type: :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # TodoListsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
 end
